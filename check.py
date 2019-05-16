58db70ed2ec25088ffdc29e0da0f56c3cfcb58dc
Tarek Ziade <tarek@ziade.org>
2019-05-16 12:20:02 +0200

    using a small square

--- check.py
+++ check.py
@@ -2,19 +2,31 @@ import moviepy.editor as mpe
 
 my_clip = mpe.VideoFileClip("noise.mp4")
 
-FPS = 24
+FPS = 24.0
+white = (255, 255, 255)
+width = 1280
+height = 720
+
+
+def almost_white(color):
+    int_color = RGB2Int(color)
+    int_white = RGB2Int(white)
+    delta = abs(int_white - int_color)
+    return delta < 256 * 256 * 256 * 0.03
+
 
 def RGB2Int(value):
     value = list(value)
     value.reverse()
     return (value[0] << 16) + (value[1] << 8) + value[2]
 
+
 def assert_frame(i, frame):
-    # we want to verify the color of the first point
-    color = frame[0][0]
-    int_color = RGB2Int(color)
-    print("found:%d, frame: %d" % (int_color, i))
+    # we want to verify we have the white square
+    x = i % width
+    y = i - (i % width)
+    assert almost_white(frame[y][x]), frame[y][x]
+
 
 for i, frame in enumerate(my_clip.iter_frames()):
     assert_frame(i, frame)
-
