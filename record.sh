288c4f93e7d41d902e0632042374db5fb53f1ef2
Tarek Ziade <tarek@ziade.org>
2019-06-10 17:44:18 +0200

    magic line

--- record.sh
+++ record.sh
@@ -1,1 +1,1 @@
-ffmpeg -f avfoundation -framerate 60 -i "1" -crf 0 -preset ultrafast output.mkv
+ffmpeg -y -f avfoundation -framerate 60 -video_size 1920x1080 -i "1:0" -vsync 0  -vcodec h264_videotoolbox -acodec mp2 -b:v 1M -b:a 192k output.mp4
